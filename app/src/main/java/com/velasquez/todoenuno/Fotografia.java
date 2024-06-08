5a74fd2bc4c04848f2c3d09b0e18e681cd6017ab
rvelasquezq <rodrigovelas2@gmail.com>
2024-06-08 00:01:55 -0500

    feat: se añadio la funcionalidad de guardar la foto que se tomo
    Fotografia.java
    
    commitLint

--- app/src/main/java/com/velasquez/todoenuno/Fotografia.java
+++ app/src/main/java/com/velasquez/todoenuno/Fotografia.java
@@ -10,6 +10,8 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.Manifest; // correct
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,11 +24,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Fotografia extends AppCompatActivity {
     ImageView imageViewGaleria;
     ImageView imageViewCamera;
-    String path;
+
+    String currentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +48,6 @@ public class Fotografia extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1000);
         }
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -50,42 +55,69 @@ public class Fotografia extends AppCompatActivity {
         });
     }//Fin onCreate
 
-    public void capturarPhoto(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            File fotoArchivo = null;
-            try {
-                fotoArchivo = guardarImage();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }//Fin catch
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); // Registra la fecha actual de la fotografia
+        String imageFileName = "Backup_" + timeStamp + "_"; // Crear una segunda imagen para respaldarla
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "my_images");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }//Fin createImageFile
 
-            if (fotoArchivo != null) {
-                Uri uri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", fotoArchivo);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-                startActivityForResult(intent, 1);
+    public void tomarFoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Indicarle a la actividad que se va a tomar la foto
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) { // Si la actividad es diferente de null
+            File photoFile = null; // Crear una variable de tipo File
+            try {
+                photoFile = createImageFile(); // Crear el archivo de la fotografia
+            } catch (IOException ex) {
+                Toast.makeText(this, "Error al crear el archivo de imagen", Toast.LENGTH_SHORT).show();
+            }
+            if (photoFile != null) { // Si la variable no es null
+                try {
+                    Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);// Recuperar la URI del archivo
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                } catch (Exception e) {
+                    Toast.makeText(this, "Error al tomar la foto" + e, Toast.LENGTH_SHORT).show();
+                }//Fin catch
             }//Fin if
         }//Fin if
-    }//Fin capturarPhoto
-
-    private File guardarImage() throws IOException {
-        String nombreFoto = "foto.jpg";
-        File directorioFotos = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File foto = File.createTempFile(nombreFoto, ".jpg", directorioFotos);
-        path = foto.getAbsolutePath();
-        return foto;
-    }//Fin guardarImage
-
-    //Método para capturar la imagen
+    }//Fin tomarFoto
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) { // Si la operación fue exitosa
-            // Recuperar la imagen
-//            Bundle extras = data.getExtras();
-//            Bitmap imgBitmap = (Bitmap) extras.get("data");
-            // Mostrar la imagen
-            Bitmap bitmap = BitmapFactory.decodeFile(path);
-            imageViewGaleria.setImageBitmap(bitmap);
-        }//Fin if
+        try {
+            if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+                // Crear un archivo a partir de la ruta de la foto
+                File imgFile = new File(currentPhotoPath);
+                if (imgFile.exists()) {
+                    // Decodificar el archivo en un Bitmap
+                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                    imageViewGaleria.setImageBitmap(myBitmap); // Establecer el Bitmap en el ImageView
+                    galleryAddPic();
+                } else {
+                    Toast.makeText(this, "No se encontró el archivo de imagen.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Error al mostrar la imagen: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
 }//Fin Fotografia
