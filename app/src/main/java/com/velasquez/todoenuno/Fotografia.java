657c874f8695d3b7ce875f3878d88992d1ef1557
rvelasquezq <rodrigovelas2@gmail.com>
2024-06-07 20:33:48 -0500

    feat: se añadio la funcion de tomar foto y mostrarlo en un imageview
    Fotografia.java
    
    commitLint

--- app/src/main/java/com/velasquez/todoenuno/Fotografia.java
+++ app/src/main/java/com/velasquez/todoenuno/Fotografia.java
@@ -1,6 +1,11 @@
 package com.velasquez.todoenuno;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Fotografia extends AppCompatActivity {
+    ImageView imageViewGaleria;
+    ImageView imageViewCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_fotografia);
+        //Encontrar componentes:
+        imageViewGaleria = findViewById(R.id.imageView_Fotografia);
+        imageViewCamera = findViewById(R.id.imageView_camera);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }//Fin onCreate
+
+    public void capturarPhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 1);
+        }//Fin if
+    }//Fin capturarPhoto
+
+    //Método para capturar la imagen
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) { // Si la operación fue exitosa
+            // Recuperar la imagen
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            // Mostrar la imagen
+            imageViewGaleria.setImageBitmap(imgBitmap);
+        }//Fin if
+    }
 }//Fin Fotografia
